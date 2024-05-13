6d5730178221ec059224f0dc50cfff6d443578eb
saika <93665867+saikadayo@users.noreply.github.com>
2024-05-13 14:07:36 -0500

    Creating pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello world');
-let favoriteFood = 'Seafood';
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 6,
+        type: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        type: ['water']
+    }
+];
