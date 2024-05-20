83b59a6acef9fddc8314e76e7acf5e4de99200ac
saika <93665867+saikadayo@users.noreply.github.com>
2024-05-20 11:39:24 -0500

    forLoop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,59 +2,30 @@ let pokemonList = [];
 
 // Starter Pokemon List
 pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 6,
-        type: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: 5,
-        type: ['water']
-    }
+    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    { name: 'Charmander', height: 0.6, type: ['fire']},
+    { name: 'Squirtle', height: 0.5, type: ['water']},
+    { name: 'Caterpie', height: 0.3, type: ['bug']},
+    { name: 'Weedle', height: 0.3, type: ['bug', 'poison']},
+    { name: 'Pidgey', height: 0.3, type: ['normal', 'flying']}
 ];
 
-// Second Pokemon List
-pokemonList2 = [
-    {
-        name: 'Caterpie',
-        height: 1,
-        type: ['bug']
-    },
-    {
-        name: 'Weedle',
-        height: 1,
-        type: ['bug', 'poison']
-    },
-    {
-        name: 'Pidgey',
-        height: 1,
-        type: ['normal', 'flying']
-    }
-];
+// Using forEach() to write Pokemon name and height if their height 7 or bigger
+pokemonList.forEach(pokemon => {
+    const isBig = pokemon.height >= 0.7;
+    document.write(output(pokemon.name, pokemon.height, isBig));
+});
 
-// For Loop to write Pokemon name and height
-for (let i = 0; i < pokemonList.length; i++) {
-    // Condition so that Pokemon size 7 or higher gets a unique message in DOM
-    if (pokemonList[i].height >= 7) {
-        document.write(`<p><span class="pokemon_name">${pokemonList[i].name}</span> <span class="pokemon_height">(height: ${pokemonList[i].height})</span> - Wow, that's big!</p>`);
-    // Else the written message in DOM is the generic name and height
-    } else {
-        document.write(`<p><span class="pokemon_name">${pokemonList[i].name}</span> <span class="pokemon_height">(height: ${pokemonList[i].height})</span></p>`);
-    }
+// Using forEach() to write Pokemon name regular size
+function printArrayDetails(list) {
+    list.forEach(pokemon => {
+        document.write("<p>" + pokemon.name + "</p>");
+        console.log(pokemon.name);
+    });
 }
 
-function printArrayDetails(list){
-    for (let i = 0; i < list.length; i++){
-      document.write("<p>" + list[i].name + "</p>")
-      console.log(list[i].name);
-    }
-  }
-  
-  printArrayDetails(pokemonList);
-  printArrayDetails(pokemonList2);  
+// Output to print the formatted list
+function output(name, height, isBig) {
+    const text = `<p><span class="pokemon_name">${name}</span> <span class="pokemon_height">(height: ${height})</span>`;
+    return isBig ? text + " - Wow, that's big!</p>" : text;
+}
